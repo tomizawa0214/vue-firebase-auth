ca4365a1ec4608ea2b63cc229f8eba5abc36dcd1
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-08-17 10:36:53 +0900

    ナビゲーションガード追加

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/components/Home";
 import Signup from "@/components/Signup";
+import Login from "@/components/Login";
+import firebase from "firebase";
 
 Vue.use(VueRouter);
 
@@ -9,18 +11,41 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: "/signup",
     name: "Signup",
     component: Signup
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    let user = firebase.auth().currentUser;
+    if (user) {
+      next();
+    } else {
+      next({
+        name: "Login"
+      });
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
