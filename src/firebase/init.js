14a411094ce242f8e8c43a765511657a2f5c5910
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-08-17 09:19:26 +0900

    firebase設定追加

---
+++ src/firebase/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDTyLr49LnWYQ461AIQh2Hm-A6UHT80NMc",
+    authDomain: "vue-firebase-auth-e084c.firebaseapp.com",
+    databaseURL: "https://vue-firebase-auth-e084c.firebaseio.com",
+    projectId: "vue-firebase-auth-e084c",
+    storageBucket: "vue-firebase-auth-e084c.appspot.com",
+    messagingSenderId: "283991060890",
+    appId: "1:283991060890:web:068d8b3f2d1cbb69de8a23"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
